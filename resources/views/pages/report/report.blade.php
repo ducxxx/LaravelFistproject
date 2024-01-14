d2d42964002c868fbdfd5be011002c1f0b89731b
TonyTran <tony.tran@it-viet.com>
2024-01-15 00:54:15 +0700

    update code

--- resources/views/pages/report/report.blade.php
+++ resources/views/pages/report/report.blade.php
@@ -6,49 +6,122 @@
     <title>Report</title>
 @endsection
 @section("body")
-    <main class="ant-layout-content css-12jzuas" style="padding: 24px; overflow: auto;">
-        <div style="display: flex; align-items: center; justify-content: center;">
-            <div style="display: flex; padding: 30px; background: rgb(255, 255, 255); border-radius: 10px;
-             box-shadow: rgba(0, 0, 0, 0.12) 0px 5px 5px;">
-                <canvas id="bar-chart-custom-tooltip"></canvas>
-                <script>
-                    // Bar chart with custom tooltip
-                    const dataBarCustomTooltip = {
-                        type: 'bar',
-                        data: {
-                            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                            datasets: [
-                                {
-                                    label: 'Traffic',
-                                    data: [30, 15, 62, 65, 61, 65, 40],
-                                },
-                            ],
-                        },
-                    };
-
-                    const optionsBarCustomTooltip = {
-                        options: {
-                            plugins: {
-                                tooltip: {
-                                    callbacks: {
-                                        label: function (context) {
-                                            let label = context.dataset.label || '';
-                                            label = `${label}: ${context.formattedValue} users`;
-                                            return label;
-                                        },
-                                    },
-                                },
-                            },
-                        },
-                    };
-
-                    new mdb.Chart(
-                        document.getElementById('bar-chart-custom-tooltip'),
-                        dataBarCustomTooltip,
-                        optionsBarCustomTooltip);
-
-                </script>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        <!-- Content Row -->
+        <div class="row col-md-12">
+            <!-- Bar Chart -->
+            <div class="card shadow col-md-12">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+                </div>
+                <div class="card-body">
+                    <div class="chart-bar">
+                        <canvas id="myBarChart"></canvas>
+                    </div>
+                </div>
             </div>
         </div>
-    </main>
+
+    </div>
+@endsection
+@section("js")
+    <!-- Page level plugins -->
+    <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('js/demo/chart-bar-demo.js') }}"></script>
+    <script>
+        var dataOrder = [
+            {
+                label: "Mượn",
+                backgroundColor: "#4e73df",
+                hoverBackgroundColor: "#2e59d9",
+                borderColor: "#4e73df",
+                data: [10, 15, 20, 25, 30, 10, 10, 15, 20, 25, 30, 10],
+            },
+            {
+                label: "Trả",
+                backgroundColor: "#bb1a32",
+                hoverBackgroundColor: "#bb1a32",
+                borderColor: "#bb1a32",
+                data: [9, 16, 8, 20, 25, 5, 9, 16, 8, 20, 25, 5],
+            }
+        ]
+        // Bar Chart Example
+        var ctx = document.getElementById("myBarChart");
+        var myBarChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                datasets: dataOrder,
+            },
+            options: {
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 25,
+                        top: 25,
+                        bottom: 0
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        time: {
+                            unit: 'month'
+                        },
+                        gridLines: {
+                            display: false,
+                            drawBorder: false
+                        },
+                        ticks: {
+                            maxTicksLimit: 12
+                        },
+                        maxBarThickness: 25,
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            min: 0,
+                            max: 100,
+                            maxTicksLimit: 12,
+                            padding: 10,
+                            // Include a dollar sign in the ticks
+                            callback: function(value, index, values) {
+                                return number_format(value);
+                            }
+                        },
+                        gridLines: {
+                            color: "rgb(234, 236, 244)",
+                            zeroLineColor: "rgb(234, 236, 244)",
+                            drawBorder: false,
+                            borderDash: [2],
+                            zeroLineBorderDash: [2]
+                        }
+                    }],
+                },
+                legend: {
+                    display: false
+                },
+                tooltips: {
+                    titleMarginBottom: 10,
+                    titleFontColor: '#6e707e',
+                    titleFontSize: 14,
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: '#dddfeb',
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
+                    callbacks: {
+                        label: function(tooltipItem, chart) {
+                            var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                            return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
+                        }
+                    }
+                },
+            }
+        });
+    </script>
+
 @endsection
